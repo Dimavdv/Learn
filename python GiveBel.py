28821739eec4f884ffc13eda45a89bee48a1d1d0
Dimavdv <82948916+Dimavdv@users.noreply.github.com>
2021-05-12 07:52:12 +0300

    Add files via upload

---
+++ python GiveBel.py
@@ -0,0 +1,14 @@
+import telebot;
+bot=telebot.TeleBot('1770021299:AAH3I8Ikq-Uv0WG9dUGy_Iillwp7FnJN-tw')
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True)
